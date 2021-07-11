031b4d976a2fd870f9d278b57fa30ef3d1fd59ec
kapros <kamil.prosowski@gmail.com>
2021-07-11 13:59:06 +0200

    add first test - failing
    skipping the part where you cannot even compile

---
+++ Update/Dice/Dice.Tests/RegularDiceTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dice.Tests
+{
+    public class RegularDiceTests
+    {
+        [Fact]
+        public void Regular_dice_max_value_is_6()
+        {
+            const int maxValue = 6;
+
+            var dice = new RegularDice();
+
+            dice.MaxValue.Should().Be(maxValue);
+        }
+    }
+}
---
+++ Update/Dice/Dice/RegularDice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    public class RegularDice
+    {
+        public object MaxValue { get; set; }
+    }
+}
