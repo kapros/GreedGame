5cd7aea60804ac8d80ffff40e67755f526c60bc2
kapros <kamil.prosowski@gmail.com>
2021-07-11 18:53:01 +0200

    add first DiceProvider test

---
+++ Update/Dice/Dice.Tests/DiceProviderTests.cs
@@ -0,0 +1,24 @@
+﻿using Dice.DiceProviders;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dice.Tests
+{
+    public class DiceProviderTests
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(6)]
+        public void Should_return_correct_amount_of_dice(int diceToReturn)
+        {
+            var provider = new RegularDiceProvider();
+            var dice = provider.RollDice(diceToReturn);
+            dice.Should().HaveCount(diceToReturn);
+        }
+    }
+}
---
+++ Update/Dice/Dice/DiceProviders/RegularDiceProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice.DiceProviders
+{
+    public class RegularDiceProvider
+    {
+        public IEnumerable<int> RollDice(int diceToReturn)
+        {
+            return null;
+        }
+    }
+}
